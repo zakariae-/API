5c103c8f923388df87ce6c526b39bbf367920fe1
Zakariae EL ALAOUI EL BELGHITI <zakariae.elalaoui@gmail.com>
2018-07-13 23:19:03 +0200

    Add JWT
    composer require lexik/jwt-authentication-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
